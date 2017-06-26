954a5d9ca09d80eb5f9891d941b4f3b631dd16cb
ngc6589 <z-gypsy@nifty.com>
2017-06-26 21:08:06 +0900

    漢字表示する部分をコーディング

--- oled.py
+++ oled.py
@@ -12,8 +12,8 @@ from PIL import ImageDraw
 from PIL import ImageFont
 
 # メタデータファイル
-# metaDataFileName = '/var/local/www/currentaudio.txt'
-metaDataFileName = 'currentaudio.txt'
+metaDataFileName = '/var/local/www/currentsong.txt'
+#metaDataFileName = 'currentaudio.txt'
 lastModTImeSave = 0
 lastModTImeNow = 0
 
@@ -49,11 +49,28 @@ while True:
 
     # メタデータファイルが変更されたら OLED に表示するデータ更新
     lastModTimeNow = os.stat(metaDataFileName).st_mtime
-    if lastModTimeNow > lastModTimeSave:
+    if lastModTimeNow > lastModTImeSave:
         # メタデータファイルを読んで変数更新
+        for line in open(metaDataFileName, 'r'):
+            itemList = line[:-1].split('=')
+            str01 = itemList[1]
+            str01 = str01.decode('UTF8')
+            print itemList[0], str01
+            if itemList[0] == 'title':
+                str01 = itemList[1]
+                str01 = str01.decode('UTF8')
+                titleStr = str01
+            if itemList[0] == 'album':
+                str01 = itemList[1]
+                str01 = str01.decode('UTF8')
+                albumStr = str01
+            if itemList[0] == 'artist':
+                str01 = itemList[1]
+                str01 = str01.decode('UTF8')
+                artistStr = str01
 
-        titleStr = '洋酒といえば、誰でも最初に思い浮かべるのがウイスキー。いわば洋酒のシンボル的な存在'
-        titleStr = titleStr.decode('UTF8')
+        #titleStr = '洋酒といえば、誰でも最初に思い浮かべるのがウイスキー。いわば洋酒のシンボル的な存在'
+        #titleStr = titleStr.decode('UTF8')
         titleImageSize = font12.getsize(titleStr)
 
         # テキスト描画イメージ幅を120ピクセルの整数倍となるようにアライメント
@@ -62,8 +79,7 @@ while True:
             rowCout = rowCount + 1
         if rowCount < 2:
             rowCount = 2
-        titleImageSize[0] = rowCount * 120
-        # titleImageSize = (titleImageSize[0], 12 + titleImageSize[1] + 12)
+        titleImageSize = (rowCount * 120, titleImageSize[1])
 
         # タイトルの文字列を描画
         titleImageWK = Image.new('1', titleImageSize)
@@ -71,13 +87,24 @@ while True:
         titledrawWK.text((0, 0), titleStr, font=font12, fill=255)
 
         # 描画した文字列を縦スクロール用イメージに貼り付ける
-        titleImage = Image.new('1', (120, (rowCount + 4 * 12)))
+        titleImage = Image.new('1', (120, ((rowCount + 4) * 12)))
         for i in range(rowCount):
-            region = titleImageWK.crop((rowCount * 120, 0, rowCount * 120 + 120, 12))
-            titleImage.paste(region, (0, rowCount * 12 + 24, 120, rowCount * 12 + 36))
+            region = titleImageWK.crop((i * 120, 0, i * 120 + 120, 12))
+            titleImage.paste(region, (0, i * 12 + 24, 120, i * 12 + 36))
 
-        lastModTimeSave = lastModTimeNow
+        # アルバムの文字列を描画
+        albumImageSize = font12.getsize(albumStr)
+        albumImageWK = Image.new('1', albumImageSize)
+        albumdrawWK = ImageDraw.Draw(albumImageWK)
+        albumdrawWK.text((0, 0), albumStr, font=font12, fill=255)
 
+        # アーティストの文字列を描画
+        artistImageSize = font12.getsize(artistStr)
+        artistImageWK = Image.new('1', artistImageSize)
+        artistdrawWK = ImageDraw.Draw(artistImageWK)
+        artistdrawWK.text((0, 0), artistStr, font=font12, fill=255)
+
+        lastModTImeSave = lastModTimeNow
 
 
     # タイトル文字イメージを OLED 用キャンバスに貼り付け
@@ -87,9 +114,18 @@ while True:
     # スクロール終了位置判定
     # カウンターの範囲をみて、スクロールしないタイミングなど実装すると見やすくなるかも
     scrollCount = scrollCount + 1
-    if scroolCount > (rowCount + 2) * 12:
-        scrollCOunt = 0
-        
+    if scrollCount > (rowCount + 2) * 12:
+        scrollCount = 0
+
+
+    # アルバム文字列をキャンバスに貼る
+    region = albumImageWK.crop((0, 0, 120, 12))
+    image.paste(region,(8, 24))
+    
+    # アーティスト文字列をキャンバスに貼る
+    region = artistImageWK.crop((0, 0, 120, 12))
+    image.paste(region,(8, 36))
+    
     # OLED に出力
     disp.image(image)
     disp.display()
