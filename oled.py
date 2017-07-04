2358769973855aa1478a92625b5aeb0ab12ce37b
ngc6589 <masahiro@em9system.com>
2017-07-04 20:00:40 +0900

    commit by mkusunoki

--- oled.py
+++ oled.py
@@ -64,6 +64,23 @@ icon3 = [
     '  *********      *********      *********      *********  '
     ]
 
+icon4 = [
+    '                                                          ',
+    '  *********      *********      *********      *********  ',
+    ' *         *    *         *    *         *    *         * ',
+    '*           *  *           *  *    ***    *  *           *',
+    '*     *     *  *           *  *   *   *   *  *           *',
+    '*    * *    *  * *       * *  *  *     *  *  *           *',
+    '*   *   *   *  *  *     *  *  * *       * *  *           *',
+    '*  *     *  *  *   *   *   *  * *       * *  *           *',
+    '* *       * *  *    * *    *  * *       * *  *           *',
+    '*           *  *     *     *  *  *     *  *  *           *',
+    '*           *  *           *  *   *   *   *  *           *',
+    '*           *  *           *  *    ***    *  *           *',
+    ' *         *    *         *    *         *    *         * ',
+    '  *********      *********      *********      *********  '
+    ]
+
 
 icon1ysize = len(icon1)
 icon1xsize = len(icon1[0])
@@ -92,9 +109,17 @@ for y in range(icon3ysize):
         if char01[x] != ' ':
             icon3image.putpixel((x, y), 1)
 
+icon4ysize = len(icon4)
+icon4xsize = len(icon4[0])
+icon4image = Image.new('1', (icon4xsize, icon4ysize))
+for y in range(icon4ysize):
+    char01 = icon4[y]
+    for x in range(icon4xsize):
+        if char01[x] != ' ':
+            icon4image.putpixel((x, y), 1)
+
 iconMode = 0
 
-#
 # Input pins:
 btnA = 17
 btnB = 27
@@ -139,9 +164,10 @@ font12 = ImageFont.truetype('/home/pi/PixelMplus12-Regular.ttf', 12, encoding='u
 # 変数
 scrollCount = 0
 rowCount = 0
+loopCount = 0
 
 try:
-    
+
     while True:
 
         # Draw a black filled box to clear the image.
@@ -204,36 +230,44 @@ try:
 
             lastModTImeSave = lastModTimeNow
 
-
-        # タイトル文字イメージを OLED 用キャンバスに貼り付け
-        region = titleImage.crop((0, scrollCount + 0, 120, scrollCount + 24))
-        image.paste(region,(8, 0))
-
-        # スクロール終了位置判定
-        # カウンターの範囲をみて、スクロールしないタイミングなど実装すると見やすくなるかも
-        scrollCount = scrollCount + 1
-        if scrollCount > (rowCount + 2) * 12:
-            scrollCount = 0
-
-
-        # アルバム文字列をキャンバスに貼る
-        region = albumImageWK.crop((0, 0, 120, 12))
-        image.paste(region,(8, 25))
-    
-        # アーティスト文字列をキャンバスに貼る
-        region = artistImageWK.crop((0, 0, 120, 12))
-        image.paste(region,(8, 37))
-
+        if btnMode != 3:
+            # タイトル文字イメージを OLED 用キャンバスに貼り付け
+            region = titleImage.crop((0, scrollCount + 0, 120, scrollCount + 24))
+            image.paste(region,(8, 0))
+            # スクロール終了位置判定
+            # カウンターの範囲をみて、スクロールしないタイミングなど実装すると見やすくなるかも
+            scrollCount = scrollCount + 1
+            if scrollCount > (rowCount + 2) * 12:
+                scrollCount = 0
+            # アルバム文字列をキャンバスに貼る
+            region = albumImageWK.crop((0, 0, 120, 12))
+            image.paste(region,(8, 25))
+            # アーティスト文字列をキャンバスに貼る
+            region = artistImageWK.crop((0, 0, 120, 12))
+            image.paste(region,(8, 37))
+        else:
+            # プレイリストを描画
+            maxPages = len(fileNames) / 4
+            for i in range(4):
+                if numFileNames > (displayPageNum * 4 + i):
+                    str01 = fileNames[displayPageNum * 4 + i]
+                    str01 = str01.decode('UTF8')
+                    if displayLineNum == i:
+                        draw.text((0, i * 12), "* " + str01, font=font12, fill=255)
+                    else:
+                        draw.text((0, i * 12), "  " + str01, font=font12, fill=255)
+                                
 
         # ボタン押されている間は処理スキップ
         btnAstatus = GPIO.input(btnA)
         btnBstatus = GPIO.input(btnB)
         btnCstatus = GPIO.input(btnC)
         btnDstatus = GPIO.input(btnD)
-        
+
         if btnPress == True:
             if btnAstatus == 1 and btnBstatus == 1 and btnCstatus == 1 and btnDstatus == 1:
                 btnPress = False
+        # ボタン判定
         else:
             if btnMode == 0:
                 if btnAstatus == 0:
@@ -245,7 +279,6 @@ try:
                 elif btnCstatus == 0:
                     #ミュート
                     os.system('mpc volume 0')
-
             elif btnMode == 1:
                 if btnAstatus == 0:
                     #再生
@@ -256,7 +289,6 @@ try:
                 elif btnCstatus == 0:
                     #停止
                     os.system('mpc stop')
-
             elif btnMode == 2:
                 if btnAstatus == 0:
                     #もどる
@@ -267,13 +299,44 @@ try:
                 elif btnCstatus == 0:
                     #電源オフ
                     os.system('sudo poweroff')
-                
+            elif btnMode == 3:
+                if btnAstatus == 0:
+                    #上のファイル
+                    if displayPageNum == 0 and displayLineNum == 0:
+                        pass
+                    else:
+                        displayLineNum = displayLineNum - 1
+                        if displayLineNum < 0:
+                            displayLineNum = 3
+                            displayPageNum = displayPageNum - 1
+                elif btnBstatus ==0:
+                    #下のファイル
+                    if (displayPageNum * 4 + displayLineNum + 1) < numFileNames:
+                        displayLineNum = displayLineNum + 1
+                        if displayLineNum > 3:
+                            if displayPageNum  == maxPages:
+                                displayLineNum = 3
+                            else:
+                                displayLineNum = 0
+                                displayPageNum = displayPageNum + 1
+                elif btnCstatus == 0:
+                    #プレイリスト決定
+                    (file,ext) = os.path.splitext(fileNames[displayPageNum * 4 + displayLineNum])
+                    str01 = "mpc load " + "'" + file + "'"
+                    os.system('mpc clear')
+                    os.system(str01)
+                    os.system('mpc play')
+                    #print str01
+
             if btnDstatus == 0:
                 btnMode = btnMode + 1
-                if btnMode > 2:
+                # プレイリスト選択モードに入るときには、ファイル一覧を更新する
+                if btnMode == 3:
+                    displayPageNum = 0
+                    displayLineNum = 0
+                if btnMode > 3:
                     btnMode = 0
-            
-                    
+        # 
         # ボタンアイコンを画面に貼る
         if btnMode == 0:
             region = icon1image.crop((0, 0, icon1xsize, icon1ysize))
@@ -284,13 +347,30 @@ try:
         if btnMode == 2:
             region = icon3image.crop((0, 0, icon3xsize, icon3ysize))
             image.paste(region,(0, 50))
-        
-    
+        if btnMode == 3:
+            region = icon4image.crop((0, 0, icon4xsize, icon4ysize))
+            image.paste(region,(0, 50))
+
+
+        # 一定時間ごとに/var/lib/mpd/playlists ファイル一覧を更新する
+        if loopCount == 0:
+            for path, dir, file in os.walk('/var/lib/mpd/playlists'):
+                fileNames = []
+                absFileNames = []
+                for fname in file:
+                    fileNames.append(fname)
+                    absFileNames.append(os.path.join(path, fname))
+                    fileNames.sort()
+                    absFileNames.sort()
+                    numFileNames = len(fileNames)
+            
         # OLED に出力
         disp.image(image)
         disp.display()
         time.sleep(0.15)
-
+        loopCount = loopCount + 1
+        if loopCount > 1000:
+            loopCount = 0
 
 except KeyboardInterrupt:
     GPIO.cleanup()
@@ -299,4 +379,3 @@ except KeyboardInterrupt:
 #
 # python 入門者のコードのため冗長な書き方が多いです。
 # Written by Masahiro Kusunoki http://mkusunoki.net http://em9system.net
-#
